8cef139f771af83fb514eb7d91c4d8f4d6d8cd6c
Hanna Haataja <hanna.haataja@cs.tamk.fi>
2019-02-13 20:20:19 +0200

    Initial commit

---
+++ src/main/java/fi/tamk/cv/generator/GeneratorApplication.java
@@ -0,0 +1,14 @@
+package fi.tamk.cv.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class, args);
+    }
+
+}
+
