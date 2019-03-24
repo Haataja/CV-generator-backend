88d732ec21311f88a42dd713bc51609b74bb07e9
Haataja <hanna.haataja@cs.tamk.fi>
2019-03-24 12:53:39 +0200

    CvController: Add Samu to copyright owners

--- src/main/java/fi/tamk/cv/generator/CvController.java
+++ src/main/java/fi/tamk/cv/generator/CvController.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Hanna Haataja <hanna.haataja@tuni.fi>. All rights reserved.
+Copyright 2019 Hanna Haataja <hanna.haataja@tuni.fi>, Samu Koivulahti <samu.koivulahti@tuni.fi>. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 following conditions are met:
