1e8e3c50a6374b9a31db7f5d64db204ef01caccc
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-30 18:50:55 +0200

    SecurityConfig: Update pattern matching regex

--- src/main/java/fi/tamk/cv/generator/SecurityConfig.java
+++ src/main/java/fi/tamk/cv/generator/SecurityConfig.java
@@ -21,40 +21,31 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package fi.tamk.cv.generator;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers("/", "/api/test")
+                .antMatchers("/", "/static/**", "/api/get/user", "/api/pdf", "/api/test")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
-        http.authorizeRequests()
-                .antMatchers("/api/get/user").permitAll()
-                .antMatchers("/api/demo").permitAll()
-                .antMatchers("/api/pdf").permitAll()
-                .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
-                .defaultSuccessUrl("/api/loginSuccess").failureUrl("/api/error");
+                .loginPage("/")
+                .defaultSuccessUrl("/api/loginSuccess")
+                .failureUrl("/api/error");
     }
 
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**");
+        web.ignoring().antMatchers("/resources/static/**");
     }
-
-
 }
