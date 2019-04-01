c8a905b8f90ad9aac88cf18bd882e37a997a7014
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-04-01 21:21:59 +0300

    SecurityConfig: Force always on sessions
    - Force default success url

--- src/main/java/fi/tamk/cv/generator/SecurityConfig.java
+++ src/main/java/fi/tamk/cv/generator/SecurityConfig.java
@@ -22,9 +22,9 @@ package fi.tamk.cv.generator;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -32,17 +32,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http    .authorizeRequests()
+        http    .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .oauth2Login()
-                .defaultSuccessUrl("/api/loginSuccess")
+                .defaultSuccessUrl("/api/loginSuccess", true)
                 .failureUrl("/api/error");
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/*.*", "/static/**");
     }
 }
