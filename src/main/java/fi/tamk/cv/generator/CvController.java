9f8dc0bba54264d5c184c24b2cfdd202225aec5f
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-04-02 00:43:25 +0300

    CvController: Update copyright notification

--- src/main/java/fi/tamk/cv/generator/CvController.java
+++ src/main/java/fi/tamk/cv/generator/CvController.java
@@ -1,5 +1,6 @@
 /*
-Copyright 2019 Hanna Haataja <hanna.haataja@tuni.fi>, Samu Koivulahti <samu.koivulahti@tuni.fi>. All rights reserved.
+Copyright 2019 Hanna Haataja <hanna.haataja@tuni.fi>, Samu Koivulahti <samu.koivulahti@tuni.fi>,
+               Joonas Lauhala <joonas.lauhala@tuni.fi>. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 following conditions are met:
@@ -37,7 +38,6 @@ import org.springframework.web.context.request.RequestContextHolder;
 import java.io.*;
 import java.security.GeneralSecurityException;
 import java.time.LocalDate;
-
 
 @RestController
 @RequestMapping("api/")
