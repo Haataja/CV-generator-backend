7749602890b0f9beb248778d4bbe8bfa8b687633
Haataja <hanna.haataja@cs.tamk.fi>
2019-04-09 12:57:02 +0300

    CvController: Add Tuukka to copyright owners

--- src/main/java/fi/tamk/cv/generator/rest/CvController.java
+++ src/main/java/fi/tamk/cv/generator/rest/CvController.java
@@ -1,6 +1,6 @@
 /*
 Copyright 2019 Hanna Haataja <hanna.haataja@tuni.fi>, Samu Koivulahti <samu.koivulahti@tuni.fi>,
-               Joonas Lauhala <joonas.lauhala@tuni.fi>. All rights reserved.
+               Joonas Lauhala <joonas.lauhala@tuni.fi>, Tuukka Juusela <tuukka.juusela@tuni.fi>. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 following conditions are met:
