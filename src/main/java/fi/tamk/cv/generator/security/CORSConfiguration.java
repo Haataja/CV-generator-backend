a5cd9e6bd5df37840d0ea5de18f9540d5563c390
Haataja <hanna.haataja@cs.tamk.fi>
2019-04-16 07:11:06 +0300

    CORSConfiguration: Remove annotation and add javadoc

--- src/main/java/fi/tamk/cv/generator/security/CORSConfiguration.java
+++ src/main/java/fi/tamk/cv/generator/security/CORSConfiguration.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+/**
+ * This configuration is needed in development, to allow contact from front development server.
+ * No need to be in use if front end is build to resources.
+ */
+// @Configuration
 public class CORSConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
